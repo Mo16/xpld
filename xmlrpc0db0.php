d87a6e7188b40fb644e1d601d25706530596564e
Mohammed Choudhury <mosminecraft4@gmail.com>
2021-04-25 21:38:46 +0100

    inital commit

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>http://wpdemo.oceanthemes.net/icos-zinnia</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="http://wpdemo.oceanthemes.net/icos-zinnia/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://wpdemo.oceanthemes.net/icos-zinnia/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://wpdemo.oceanthemes.net/icos-zinnia/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="http://wpdemo.oceanthemes.net/icos-zinnia/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="http://wpdemo.oceanthemes.net/icos-zinnia/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
